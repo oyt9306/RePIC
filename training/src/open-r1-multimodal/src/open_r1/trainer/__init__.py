567cff983703e8d217da44de7aee442a0ea9067e
Daniel123 <41467632+oyt9306@users.noreply.github.com>
2025-09-27 22:16:19 +0900

    Update __init__.py

--- training/src/open-r1-multimodal/src/open_r1/trainer/__init__.py
+++ training/src/open-r1-multimodal/src/open_r1/trainer/__init__.py
@@ -1,8 +1,4 @@
 from .grpo_trainer import Qwen2VLGRPOTrainer
 from .grpo_config import GRPOConfig
 
-from .ppo_trainer import Qwen2VLPPOTrainer
-from .ppo_config import PPOConfig
-
-
-__all__ = ["Qwen2VLGRPOTrainer", "Qwen2VLPPOTrainer"]
+__all__ = ["Qwen2VLGRPOTrainer"]
